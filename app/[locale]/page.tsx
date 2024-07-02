3340250be89352a3f81a17661cbd2ccedff5059a
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-07-02 19:48:47 +0300

    solve some problems

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import { useTranslations } from "next-intl";
-
+import { unstable_setRequestLocale } from 'next-intl/server';
 import Visit from "@/components/come-visit";
 import Cover from "@/components/Cover";
 import Footer from "@/components/footer";
 import CarouselSpacing from "@/components/popularFood";
 import Reservation from "@/components/Reservation";
 
-export default function Home() {
+export default function Home({ params: { locale } }: Readonly<{ params: { locale: string } }>) {
+  unstable_setRequestLocale(locale);
   const t = useTranslations("Index");
   return (
     <>
