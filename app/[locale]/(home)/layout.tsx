a9f8d6f83662a911699d3dd22aa18f9f0a06e8cc
ahmedakram14 <ahmedelkhergawy98@gmail.com>
2024-07-07 23:14:00 +0300

    fix changes

--- app/[locale]/(home)/layout.tsx
+++ app/[locale]/(home)/layout.tsx
@@ -2,12 +2,13 @@ import '../../globals.css';
 
 import React from 'react'; // Import React explicitly
 
-import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { Inter } from 'next/font/google';
 
 import Header from '@/components/Header';
 import { AuthContextProvider } from '@/context/AuthContext';
-import { NextIntlClientProvider } from 'next-intl';
 
+const inter = Inter({ subsets: ["latin"] });
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -19,9 +20,8 @@ export default async function Layout({
   params: { locale },
 }: Readonly<RootLayoutProps>) {
   // Determine text direction based on locale
-  // const direction = locale === "ar" ? "rtl" : "ltr";
+  const direction = locale === "ar" ? "rtl" : "ltr";
   unstable_setRequestLocale(locale);
-  const messages = await getMessages();
 
   return (
     // // <html lang={locale}>
@@ -36,23 +36,34 @@ export default async function Layout({
     // //     `}</style>
     // //   </head>
     //   {/* <body> */}
-    <NextIntlClientProvider messages={messages}>
-      <AuthContextProvider>
-
-        <Header locale={locale} />
-        <main>
-          {React.Children.map(children, (child) =>
-            React.isValidElement(child)
-              ? React.cloneElement(child as React.ReactElement, {
-                className: `${child.props.className ? child.props.className + " " : ""
-                  } ltr-component`,
-              })
-              : child
-          )}
-        </main>
-      </AuthContextProvider>
-    </NextIntlClientProvider>
-
+    <AuthContextProvider>
+      <html lang={locale}>
+        <head>
+          <style>{`body { direction: ${direction}; }`}</style>
+          <style>{`${inter.style}`}</style>
+          <style>{`
+          .ltr-component {
+            direction: ltr !important; /* Force ltr direction */
+            /* Add any other specific styles */
+          }
+        `}</style>
+        </head>
+        <body>
+          <Header />
+          <main>
+            {React.Children.map(children, (child) =>
+              React.isValidElement(child)
+                ? React.cloneElement(child as React.ReactElement, {
+                    className: `${
+                      child.props.className ? child.props.className + " " : ""
+                    } ltr-component`,
+                  })
+                : child
+            )}
+          </main>
+        </body>
+      </html>
+    </AuthContextProvider>
 
     //   {/* </body>
     // </html> */}
